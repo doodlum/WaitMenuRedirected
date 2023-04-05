8304c465c762619c9e60b38e560f33a9c296ed97
Tim <15017472+doodlum@users.noreply.github.com>
2023-04-05 12:28:50 +0100

    Beast form hotfix

--- src/Hook.cpp
+++ src/Hook.cpp
@@ -24,37 +24,39 @@ namespace WaitMenuRedirected
 
 	EventResult MenuControlsHook::ProcessEvent(RE::MenuControls* a_menuControls, RE::InputEvent* const* a_event, RE::BSTEventSource<RE::InputEvent*>* a_eventSource)
 	{
-		const auto ui = RE::UI::GetSingleton();
+		if (!a_menuControls->InBeastForm()) {
+			const auto ui = RE::UI::GetSingleton();
 
-		auto IsTopMostInteractionMenu = [](const RE::UI* ui, const RE::GPtr<RE::IMenu>& iMenu) -> bool {
-			for (auto iter = ui->menuStack.end() - 1; iter >= ui->menuStack.begin(); iter--) {
-				auto menu = iter->get();
-				if (menu && (menu->inputContext.get() < RE::IMenu::Context::kTotal || menu->PausesGame() || menu->UsesCursor())) {
-					return menu == iMenu.get();
+			auto IsTopMostInteractionMenu = [](const RE::UI* ui, const RE::GPtr<RE::IMenu>& iMenu) -> bool {
+				for (auto iter = ui->menuStack.end() - 1; iter >= ui->menuStack.begin(); iter--) {
+					auto menu = iter->get();
+					if (menu && (menu->inputContext.get() < RE::IMenu::Context::kTotal || menu->PausesGame() || menu->UsesCursor())) {
+						return menu == iMenu.get();
+					}
 				}
-			}
 
-			return false;
-		};
+				return false;
+			};
 
-		const auto menuStringsHolder = RE::InterfaceStrings::GetSingleton();
-		const auto tweenMenu = ui->GetMenu<RE::TweenMenu>(menuStringsHolder->tweenMenu);
+			const auto menuStringsHolder = RE::InterfaceStrings::GetSingleton();
+			const auto tweenMenu = ui->GetMenu<RE::TweenMenu>(menuStringsHolder->tweenMenu);
 
-		if (!a_menuControls->remapMode && !a_menuControls->isProcessing && tweenMenu && tweenMenu->OnStack() && IsTopMostInteractionMenu(ui, tweenMenu) && !ui->IsMenuOpen(menuStringsHolder->inventoryMenu)) {
-			for (auto event = *a_event; event; event = event->next) {
-				if (event->eventType != RE::INPUT_EVENT_TYPE::kButton)
-					continue;
+			if (!a_menuControls->remapMode && !a_menuControls->isProcessing && tweenMenu && tweenMenu->OnStack() && IsTopMostInteractionMenu(ui, tweenMenu) && !ui->IsMenuOpen(menuStringsHolder->inventoryMenu)) {
+				for (auto event = *a_event; event; event = event->next) {
+					if (event->eventType != RE::INPUT_EVENT_TYPE::kButton)
+						continue;
 
-				const auto button = event->AsButtonEvent();
-				if (!button || !button->IsDown())
-					continue;
+					const auto button = event->AsButtonEvent();
+					if (!button || !button->IsDown())
+						continue;
 
-				const auto userEventHolder = RE::UserEvents::GetSingleton();
+					const auto userEventHolder = RE::UserEvents::GetSingleton();
 
-				if (_strcmpi(button->QUserEvent().c_str(), userEventHolder->wait.c_str()) == 0) {
-					TweenMenu__CloseTweenMenu();
-					SleepWaitMenu__OpenSleepWaitMenu(false);
-					return EventResult::kStop;
+					if (_strcmpi(button->QUserEvent().c_str(), userEventHolder->wait.c_str()) == 0) {
+						TweenMenu__CloseTweenMenu();
+						SleepWaitMenu__OpenSleepWaitMenu(false);
+						return EventResult::kStop;
+					}
 				}
 			}
 		}
